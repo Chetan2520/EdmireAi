a35cd7a1069b2515f25d1e6ab55c49ec5abe7889
Chetan Manker <chetanmanker67@gmail.com>
2025-10-24 17:43:03 +0530

    Faq spelling change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import Video from './Video'
 import VedantuComparison from './VedantuComparison'
-import Faq from './faq'
+import Faq from './Faq'
 import HelpSection from './CTA'
 import Footer from './Footer'
 
